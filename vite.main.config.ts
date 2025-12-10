592ad65092ed145b669c4ea4bc5768419228aab9
jayc <pummasinpp@gmail.com>
2025-12-10 18:43:19 +0700

    first init connect to postgres db select by firstname and hn query

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+    rollupOptions: {
+      // 1. Add this 'external' section
+      external: [
+        'pg', 
+        'pg-hstore', // Often needed if you use Sequelize, but good to include just in case
+      ], 
+    },
+  },
+});
