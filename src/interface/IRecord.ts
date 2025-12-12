905402d847bc656c9a9b31e6c699e7eb8bd63590
Karn Suksomkit <karnssk123@gmail.com>
2025-12-12 15:36:52 +0700

    add: IRecordChildParent

--- src/interface/IRecord.ts
+++ src/interface/IRecord.ts
@@ -9,3 +9,18 @@ export default interface IRecord {
     r2?: number[] | null
     r3?: number[] | null
 }
+
+export interface IRecordChildParent {
+    c_hn?: string | null
+    c_firstname?: string | null
+    c_lastname?: string| null
+    c_age?: number | null
+    c_sex?: string | null
+    c_dob?: Date | null
+    p_hn?: string | null
+    p_firstname?: string | null
+    p_lastname?: string| null
+    p_age?: number | null
+    p_sex?: string | null
+    p_dob?: Date | null
+}
