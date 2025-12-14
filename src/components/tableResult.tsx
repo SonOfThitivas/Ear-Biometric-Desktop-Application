b16166da35a31a0efbb69753e5fdf36c35581796
Karn Suksomkit <karnssk123@gmail.com>
2025-12-14 20:07:35 +0700

    edit: wait for merging database

--- src/components/tableResult.tsx
+++ src/components/tableResult.tsx
@@ -1,57 +1,116 @@
 import React from 'react';
 
-import { Table } from '@mantine/core';
+import { 
+    Box,
+    Table,
+    Title
+} from '@mantine/core';
 
-import IResult from '../interface/IResult';
+import {IRecordChildParent} from '../interface/IRecord';
+import dayjs from 'dayjs';
 
+const recordInit: IRecordChildParent = {
+    child_hn: "",
+    child_fname: "",
+    child_lname: "",
+    child_age: null,
+    child_sex: "",
+    child_dob: null,
+    parent_hn: "",
+    parent_fname: "",
+    parent_lname: "",
+    parent_age: null,
+    parent_sex: "",
+    parent_dob: null
+}
 
 // table result component
-const TableResult = ({
-    hn=null,
-    firstname=null,
-    lastname=null,
-    age=null,
-    sex=null,
-    dob=null
-}: IResult) => {
-
+const TableResult = ({record=recordInit}:
+    {record:IRecordChildParent}) => {
 
     return(
-        <Table.ScrollContainer minWidth={"25%"}>
-            <Table variant="vertical" layout="auto" withTableBorder>
-                <Table.Tbody >
-                    <Table.Tr>
-                        <Table.Th maw={"25%"}>hn</Table.Th>
-                        <Table.Td maw={"75%"}>{hn}</Table.Td>
-                    </Table.Tr>
+        <>
+            <Box component='div' bdrs={"sm"} w={"100%"}>
+                <Title order={4}>Child</Title>
+                <Table.ScrollContainer minWidth={"25%"}>
+                    <Table variant="vertical" layout="auto" withTableBorder>
+                        <Table.Tbody >
+                            <Table.Tr>
+                                <Table.Th maw={"25%"}>hn</Table.Th>
+                                <Table.Td maw={"75%"}>{record.child_hn}</Table.Td>
+                            </Table.Tr>
 
-                    <Table.Tr>
-                        <Table.Th w={"25%"}>firstname</Table.Th>
-                        <Table.Td w={"75%"}>{firstname}</Table.Td>
-                    </Table.Tr>
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>firstname</Table.Th>
+                                <Table.Td w={"75%"}>{record.child_fname}</Table.Td>
+                            </Table.Tr>
 
-                    <Table.Tr>
-                        <Table.Th w={"25%"}>lastname</Table.Th>
-                        <Table.Td w={"75%"}>{lastname}</Table.Td>
-                    </Table.Tr>
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>lastname</Table.Th>
+                                <Table.Td w={"75%"}>{record.child_lname}</Table.Td>
+                            </Table.Tr>
 
-                    <Table.Tr>
-                        <Table.Th w={"25%"}>age</Table.Th>
-                        <Table.Td w={"75%"}>{age}</Table.Td>
-                    </Table.Tr>
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>age</Table.Th>
+                                <Table.Td w={"75%"}>{record.child_age}</Table.Td>
+                            </Table.Tr>
 
-                    <Table.Tr>
-                        <Table.Th w={"25%"}>sex</Table.Th>
-                        <Table.Td w={"75%"}>{sex}</Table.Td>
-                    </Table.Tr>
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>sex</Table.Th>
+                                <Table.Td w={"75%"}>{record.child_sex}</Table.Td>
+                            </Table.Tr>
 
-                    <Table.Tr>
-                        <Table.Th w={"25%"}>dob</Table.Th>
-                        <Table.Td w={"75%"}>{dob}</Table.Td>
-                    </Table.Tr>
-                </Table.Tbody>
-            </Table>
-        </Table.ScrollContainer>
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>dob</Table.Th>
+                                <Table.Td w={"75%"}>{
+                                    record.child_dob !== null ? dayjs(record.child_dob).format("DD MMM YYYY") : null
+                                }</Table.Td>
+                            </Table.Tr>
+                        </Table.Tbody>
+                    </Table>
+                </Table.ScrollContainer>
+            </Box>
+            <Box component='div' bdrs={"sm"} w={"100%"}>
+                <Title order={4}>Parent</Title>
+                <Table.ScrollContainer minWidth={"25%"}>
+                    <Table variant="vertical" layout="auto" withTableBorder>
+                        <Table.Tbody >
+                            <Table.Tr>
+                                <Table.Th maw={"25%"}>hn</Table.Th>
+                                <Table.Td maw={"75%"}>{record.parent_hn}</Table.Td>
+                            </Table.Tr>
+
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>firstname</Table.Th>
+                                <Table.Td w={"75%"}>{record.parent_fname}</Table.Td>
+                            </Table.Tr>
+
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>lastname</Table.Th>
+                                <Table.Td w={"75%"}>{record.parent_lname}</Table.Td>
+                            </Table.Tr>
+
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>age</Table.Th>
+                                <Table.Td w={"75%"}>{record.parent_age}</Table.Td>
+                            </Table.Tr>
+
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>sex</Table.Th>
+                                <Table.Td w={"75%"}>{record.parent_sex}</Table.Td>
+                            </Table.Tr>
+
+                            <Table.Tr>
+                                <Table.Th w={"25%"}>dob</Table.Th>
+                                <Table.Td w={"75%"}>{
+                                    record.parent_dob !== null ? dayjs(record.parent_dob).format("DD MMM YYYY") : null
+                                }</Table.Td>
+                            </Table.Tr>
+                        </Table.Tbody>
+                    </Table>
+                </Table.ScrollContainer>
+            </Box>
+        </>
     )
 }
 
--- src/identify.tsx
+++ src/identify.tsx
@@ -3,110 +3,191 @@ import {
     Flex, 
     Group,
     Box, 
-    Select,
     Button,
     Text,
     Switch,
+    Alert,
+    Transition
 } from '@mantine/core'
 
 import TableResult from './components/tableResult'
-import IResult from "./interface/IResult"
+import {IRecordChildParent} from "./interface/IRecord"
+import { TbAlertCircle } from "react-icons/tb"
+
+const recordInit: IRecordChildParent = {
+    child_hn: "",
+    child_fname: "",
+    child_lname: "",
+    child_age: null,
+    child_sex: "",
+    child_dob: null,
+    parent_hn: "",
+    parent_fname: "",
+    parent_lname: "",
+    parent_age: null,
+    parent_sex: "",
+    parent_dob: null
+}
 
 export default function Identify() {
-    const [camera, setCamera] = React.useState<string>("")
-    const [mode, setMode] = React.useState<string>("child")
+    const [patient, setPatient] = React.useState<string>("child")
+    const [childParentRecord, setChildParentRecord] = React.useState<IRecordChildParent>(recordInit)
 
+    const tbAlertCircle = <TbAlertCircle/>
+    const [alertBox, setAlertBox] = React.useState<boolean>(false)  // alert error
+    const [alertTitle, setAlertTitile] = React.useState<string>("") // alert tilte
+    const [alertMsg, setAlertMsg] = React.useState<string>("")  // alert message
+    const [colorAlert, setColorAlert] = React.useState<string>("red")
+    const [loading, setLoading] = React.useState<boolean>(false) // loading icon when click
 
-    const childResult: IResult = {
-        hn: "123456789",
-        firstname: "Somsak",
-        lastname: "Meesub",
-        age: 1,
-        sex: "M",
-        dob: "1 January 2025",
+     // handle transition and alert
+    const handleTransition = () => {
+        const timeout = setTimeout((e)=>{
+            setAlertMsg("")
+            setAlertBox(false)
+            clearTimeout(timeout)
+        }, 5000)
     }
 
-    const parentResult: IResult = {
-        hn: "987654321",
-        firstname: "Somsri",
-        lastname: "Meesub",
-        age: 31,
-        sex: "F",
-        dob: "1 January 1995",
+    const handlePatientSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.currentTarget.value === "parent") setPatient("child")
+        else setPatient("parent")
     }
 
-  return (
-    <Flex
-        gap="sm"
-        justify="center"
-        direction="row"
-        p="xs"
-        w={"100%"}
-    >   
-        {/* Section Input*/}
-        <Box w={"30%"} maw={"30%"}>
-            {/* combobox */}
-            <Box component='div'>
-                 <Switch
-                    defaultChecked
-                    labelPosition="left"
-                    label="Patient Mode"
-                    size="xl"
-                    radius="xs"
-                    onLabel="Child"
-                    offLabel="Parent"
-                    p="sm"
-                />
-                {/* button */}
-                <Group grow justify={"space-between"} m={"sm"}>
-                    <Button variant='filled' color='blue'>Detect</Button>
-                    <Button variant='filled' color='green'>Confirm</Button>
-                    <Button variant='filled' color='yellow'>Reset</Button>
-                </Group>
+    const handleReset = () => {
+        setChildParentRecord(recordInit)
+    }
+
+    // TODO: camera detection and query record from database
+    const handleDetect = async () => {
+        setLoading(true)
+
+        const vector:number | null = null;
+        let res: {hn?:string , distance?:number} | null = null
+
+        try {
+            if (vector === null) {
+                // detection faild
+                throw new Error("Detection failed. Please, try again.")
+            } else if (patient !== "child" && patient !== "parent") {
+                // schematic error
+                throw new Error("Something went wrong. Please, try again.")
+            }
+
+            if (patient === "child") {
+                // find child hn
+                res = await window.electronAPI.findClosestChild(vector)
+            }
+            else {
+                // find parent hn
+                res = await window.electronAPI.findClosestParent(vector)
+            }
+
+            // get hn
+            const hn:string = res.hn
+
+            const data:Array<IRecordChildParent> = await window.electronAPI.searchByHN (hn)
+
+            if (data.length > 0){
+                // success
+                setChildParentRecord(data[0])
+                setAlertBox(true);
+                setAlertTitile("Success")
+                setAlertMsg("Detection Successfully")
+                setColorAlert("green")
+            } else {
+                throw new Error("Detection unsuccessfully, something went wrong. Please, try again.")
+            }
+
+        } catch (err){
+            setAlertBox(true);
+            setAlertTitile("Error")
+            setAlertMsg(err.message);
+            setColorAlert("red")
+        }
+
+        setLoading(false)
+    }
+
+    return (
+        <Flex
+            gap="sm"
+            justify="center"
+            direction="row"
+            p="xs"
+            w={"100%"}
+        >   
+            {/* Section Input*/}
+            <Box w={"30%"} maw={"30%"}>
+                {/* combobox */}
+                <Box component='div'>
+                    <Switch
+                        defaultChecked
+                        labelPosition="left"
+                        label="Patient Mode"
+                        size="xl"
+                        radius="xs"
+                        onLabel="Child"
+                        offLabel="Parent"
+                        p="sm"
+                        value={patient}
+                        onChange={(event)=>handlePatientSwitch(event)}
+                    />
+                    {/* button */}
+                    <Group grow justify={"space-between"} m={"sm"}>
+                        <Button variant='filled' color='blue' onClick={handleDetect} loading={loading}>Detect</Button>
+                        <Button variant='filled' color='yellow' onClick={handleReset}>Reset</Button>
+                    </Group>
+                </Box>
+
+                {/* result */}
+                <Flex w={"100%"} maw={"100%"} direction={"column"} align={"center"} p={"sm"}>
+                    <Text size='xl' fw={500}>Result</Text>
+                    <TableResult record={childParentRecord}/>
+                </Flex>
             </Box>
 
-            {/* result */}
-            <Flex w={"100%"} maw={"100%"} direction={"column"} align={"center"} p={"sm"}>
-                <Text size='xl' fw={500}>Result</Text>
-                <Box component='div' bdrs={"sm"} w={"100%"}>
-                    <Text size='sm' fw={500}>Child</Text>
-                    <TableResult 
-                        hn={childResult.hn}
-                        firstname={childResult.firstname}
-                        lastname={childResult.lastname}
-                        age={childResult.age}
-                        sex={childResult.sex}
-                        dob={childResult.dob}
-                    />
-                </Box>
-                <Box component='div' bdrs={"sm"} w={"100%"}>
-                    <Text size='sm' fw={500}>Parent</Text>
-                    <TableResult
-                        hn={parentResult.hn}
-                        firstname={parentResult.firstname}
-                        lastname={parentResult.lastname}
-                        age={parentResult.age}
-                        sex={parentResult.sex}
-                        dob={parentResult.dob}
-                    />
-                </Box>
-            </Flex>
-        </Box>
 
+            {/* Section Camera */}
+            <Box 
+                component='div' 
+                bd={"2px black solid"} 
+                bdrs={"sm"}
+                w={"70%"}
+                maw={"70%"}
+                // h={"100svh"} 
+                p={"sm"}
+            >
+                <Text size='md' fw={500}>Camera</Text>
+                
+            </Box>
 
-        {/* Section Camera */}
-        <Box 
-            component='div' 
-            bd={"2px black solid"} 
-            bdrs={"sm"}
-            w={"70%"}
-            maw={"70%"}
-            // h={"100svh"} 
-            p={"sm"}
-        >
-            <Text size='md' fw={500}>Camera</Text>
-            
-        </Box>
-    </Flex>
-  )
+            {/* alert when error */}
+            <Transition
+                mounted={alertBox}
+                transition="fade-left"
+                duration={400}
+                timingFunction="ease"
+                keepMounted
+                onEntered={handleTransition}
+            >
+                {(styles) => 
+                <Alert
+                    pos={"fixed"}
+                    w={"25%"}
+                    right={"1rem"}
+                    bottom={"1rem"}
+                    variant="filled" 
+                    color={colorAlert} 
+                    title={alertTitle}
+                    icon={tbAlertCircle}
+                    onClose={()=>setAlertBox(false)}
+                    withCloseButton
+                    style={styles}
+                >
+                    {alertMsg}
+                </Alert>}
+            </Transition>
+        </Flex>
+    )
 }
