79a80355077d4db60ca2ca3b081060605b22f6df
pancreasz <pummasinpp@gmail.com>
2026-03-24 15:15:00 +0700

    voice fix

--- src/components/voicePlayer.tsx
+++ src/components/voicePlayer.tsx
@@ -1,27 +1,40 @@
+import Start_Detection from "../assets/Start Detection.mp3"
+import Detection_finishes from "../assets/Detection finishes.mp3"
+import Start_Capture from "../assets/Start Capture.mp3"
+import Capturing_Finishes from "../assets/Capturing Finishes.mp3"
+import First_Capture from "../assets/First Capture.mp3"
+import Second_Capture from "../assets/Second Capture.mp3"
+import Third_Capture from "../assets/Third Capture.mp3"
+import one from "../assets/one.mp3"
+import two from "../assets/two.mp3"
+import three from "../assets/three.mp3"
+import four from "../assets/four.mp3"
+import five from "../assets/five.mp3"
+
 // start detection
 export const handleStartDetection = (load: Function) => {
-    load("/public/voice/Start Detection.mp3", {
+    load(Start_Detection, {
         autoplay: true,
         initialVolume: 1.0,
     })
 }
 // detection finishes
 export const handleDetectionFinish = (load: Function) => {
-    load("/public/voice/Detection finishes.mp3", {
+    load(Detection_finishes, {
         autoplay: true,
         initialVolume: 1.0,
     })
 }
 // start capture
 export const handleStartCapture = (load: Function) => {
-    load("/public/voice/Start Capture.mp3", {
+    load(Start_Capture, {
         autoplay: true,
         initialVolume: 1.0,
     })
 }
 // capturing finishes
 export const handleCaptureFinish = (load: Function) => {
-    load("/public/voice/Capturing Finishes.mp3", {
+    load(Capturing_Finishes, {
         autoplay: true,
         initialVolume: 1.0,
     })
@@ -31,19 +44,19 @@ export const handleCaptureAt = (load: Function, no: number) => {
     switch (no){
         // first caputure
         case 1:
-            load("/public/voice/First Capture.mp3", {
+            load(First_Capture, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         case 2:
-            load("/public/voice/Second Capture.mp3", {
+            load(Second_Capture, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         case 3:
-            load("/public/voice/Third Capture.mp3", {
+            load(Third_Capture, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
@@ -56,35 +69,35 @@ export const handleCount = (load:Function, no: number) => {
     switch (no) {
         // one
         case 1 :
-            load("/public/voice/one.mp3", {
+            load(one, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         // two
         case 2:
-            load("/public/voice/two.mp3", {
+            load(two, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         // three
         case 3:
-            load("/public/voice/three.mp3", {
+            load(three, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         // four
         case 4:
-            load("/public/voice/four.mp3", {
+            load(four, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
             break
         // five
         case 5:
-            load("/public/voice/five.mp3", {
+            load(five, {
                 autoplay: true,
                 initialVolume: 1.0,
             })
